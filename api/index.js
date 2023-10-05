64c03aeb8fd013630481fea48112e390177a4b1d
Ildi Dvorani <dvoranii@ildidev.com>
2023-10-05 17:26:23 -0400

    fixed package.json file

--- api/index.js
+++ api/index.js
@@ -13,4 +13,9 @@ app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "frontend", "index.html"));
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 module.exports = app;
